3d1bf1e46039b86e999537def8334359bc26a619
yixing <royalcheng99@gmail.com>
2023-03-11 23:59:01 +0800

    Added files

---
+++ css/register.css
@@ -0,0 +1,58 @@
+
+<?php
+    include "head.inc.php";
+?>
+
+<body>
+<?php
+
+    include "nav.inc.php";
+    ?>  
+<main class ="container">
+    <h1>Member Registration</h1>
+    <p>
+        For existing members, please go to the 
+        <a href ="#">Sign in page</a>.
+    </p>
+    <form action ="process_register.php" method="post" nonvalidate> 
+    <div class = "form-group"> 
+        <label for ="fname">First Name:</label>
+        <input class="form-control" id="fname" name ="fname" type="text"
+               placeholder="Enter First name">
+    </div>
+    <div class = "form-group"> 
+        <label for ="lname">Last Name:</label>
+        <input class="form-control" id="lname" maxlength = "45" name ="lname" type="text"
+               placeholder="Enter last name">
+    </div>
+    <div class = "form-group"> 
+        <label for ="email">Email:</label>
+        <input class="form-control" id="email" name ="email" type ="email"
+               placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for ="pwd">Password:</label>
+        <input class="form-control" id="pwd" name ="pwd" type="password"
+               placeholder="Enter password">
+    </div>
+    <div class = "form-group"> 
+        <label for ="pwd_confirm">Confirm password:</label>
+        <input class="form-control" id="pwd_confirm" name ="pwd_confirm" type="password"
+               placeholder="Confirm password">
+    </div>
+        <div class ="form-check">
+        <label> 
+            <input type="checkbox" name ="agree">
+            Agree to terms and conditions.
+        </label>
+        </div>
+    <div class = "form-group"> 
+        <button  class="btn btn-primary" type = "submit">Submit</button>
+    </div> 
+    </form>
+</main>
+<?php
+    include "footer.inc.php";
+?>
+</body>
+
