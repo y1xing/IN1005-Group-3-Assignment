15056797bf0e98f514a853521ea81a8bf0ba0a45
yixing <royalcheng99@gmail.com>
2023-03-15 09:36:50 +0800

    Changed head php

--- head.inc.php
+++ head.inc.php
@@ -17,6 +17,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css" type="text/css">
+
+    <?php
+
+
+    echo "<link rel='stylesheet' href='$css_file' type='text/css'>"
+
+    ?>
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="preconnect" href="https://fonts.googleapis.com">
