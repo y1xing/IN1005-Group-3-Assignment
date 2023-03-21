83a96dbf4dff0da229a3c0c906a9d57712f858ee
yixing <royalcheng99@gmail.com>
2023-03-21 18:39:28 +0800

    products page UI

--- head.inc.php
+++ head.inc.php
@@ -12,11 +12,17 @@
     <script defer src="js/nav.js">
 
     </script>
+    <script defer src="js/slider.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+
     <title>World of Pets</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css" type="text/css">
+
+
 
 
 
