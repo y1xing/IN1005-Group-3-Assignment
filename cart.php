b795c5b2a93b39e7508df15085de62c1c1acdbf5
zekphos <59187615+zekphos@users.noreply.github.com>
2023-03-30 14:30:11 +0800

    small changes

--- cart.php
+++ cart.php
@@ -25,7 +25,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
     <script defer src="js/nav.js">
 
     </script>
-    <title>Cubeworld</title>
+    <title>Cart</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
